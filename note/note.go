33b5e0257f0c1d73d9f801c019e671102f4dd89f
Adriano Orioli <orioli.adriano@gmail.com>
2016-01-23 16:26:35 +0100

    added notes

---
+++ note/note.go
@@ -0,0 +1,54 @@
+package note
+
+import "math"
+
+const (
+	a4Frequency float64 = 440.0
+	magic       float64 = 1.0594630943592952645618252949463417007792043174941856
+
+	// Musical scale halfstep values
+	C = iota
+	CIS
+	D
+	DIS
+	E
+	F
+	FIS
+	G
+	GIS
+	A
+	AIS
+	B
+)
+
+var (
+	a4 = Note{
+		Note:   A,
+		Octave: 4,
+	}
+)
+
+// Note represents a piano key note with a combination of a note and it's Octave
+type Note struct {
+	Note   int
+	Octave int
+}
+
+// AddHalfSteps adds a number of half steps and returns a new note
+func (n Note) AddHalfSteps(hs int) Note {
+	t := n.Note + hs
+	n.Note = t % 12
+	n.Octave = n.Octave + t/12
+	return n
+}
+
+// Frequency of the note
+//
+// https://en.wikipedia.org/wiki/Piano_key_frequencies
+func (n Note) Frequency() float64 {
+	return a4Frequency * math.Pow(magic, float64(halfStepDistance(a4, n)))
+}
+
+func halfStepDistance(from, to Note) int {
+	return (to.Octave-from.Octave)*12 + (to.Note - from.Note)
+}
---
+++ note/note_test.go
@@ -0,0 +1,32 @@
+package note
+
+import (
+	"testing"
+
+	"github.com/smartystreets/goconvey/convey"
+)
+
+func TestAddHalfStep(t *testing.T) {
+	convey.Convey("Given value", t, func() {
+		n := Note{Note: A, Octave: 4}
+		expected := Note{Note: C, Octave: 5}
+
+		convey.So(n.AddHalfSteps(3), convey.ShouldResemble, expected)
+	})
+}
+
+func TestHalfStepDistance(t *testing.T) {
+	convey.Convey("Given value", t, func() {
+		expected := 3
+
+		convey.So(halfStepDistance(a4, Note{Note: C, Octave: 5}), convey.ShouldEqual, expected)
+	})
+}
+
+func TestFrequency(t *testing.T) {
+	convey.Convey("Given value", t, func() {
+		actual := Note{Note: C, Octave: 5}.Frequency()
+
+		convey.So(actual, convey.ShouldAlmostEqual, 523.251, 0.001)
+	})
+}
